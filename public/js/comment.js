0932cd613c8e384a6906ada78d0a19e438ee65c6
gannacon <cwgannaway@gmail.com>
2021-06-20 18:54:05 -0500

    working comment

---
+++ public/js/comment.js
@@ -0,0 +1,25 @@
+const newCommentHandler = async (event) => {
+  event.preventDefault();
+
+  const post_id = event.target.getAttribute('data-id');
+
+  const content = document.querySelector('#comment-content').value.trim();
+
+  const response = await fetch(`/api/comments`, {
+    method: 'POST',
+    body: JSON.stringify({ post_id, content }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert('Failed to create comment');
+  }
+};
+
+document
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', newCommentHandler);
